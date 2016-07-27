245c2bb83386737c5811f1447280c34fe337e5c4
Lars-Erik Bruce <lebchen@gmail.com>
2016-07-27 22:38:23 +0200

    Remove debug loggin statements.

--- omdb.js
+++ omdb.js
@@ -2,15 +2,12 @@ const omdb = new (require('omdb-api-client'))()
 
 module.exports = function setupUrlSnatcher(client) {
 	return function urlSnatcher(nick, to, text) {
-		console.log('imdb shit')
 		if(!/^\?[oi]mdb/.test(text)) return
 
 		let args = text.trim().split(' ')
 		if(args.length === 1) return
 
 		omdb({t: args.splice(1).join(' ')}).list().then(function(movie) {
-			console.log(movie);
-
 			let strb = []
 
 			strb.push(movie.title)
@@ -21,7 +18,6 @@ module.exports = function setupUrlSnatcher(client) {
 			client.say(to, strb.join(' '))
 			client.say(to, movie.plot.substring(0, 250))
 		}).catch(function(err) {
-			console.log(err)
 			client.say(to, 'Error retrieving movie information based on title from omdb.')
 		});
 	}
